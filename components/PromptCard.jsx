089c759a95ce59fb2e29e5be4b68f655ca70c31d
Robert <r.s.garofalo87@gmail.com>
2023-06-11 10:38:04 +1000

    add copy prompt card to clipboard functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,12 @@ import { usePathname, useRouter } from 'next/navigation'
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   
   const [copied, setCopied] = useState('')
+
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(""), 3000)
+  }
   
   return (
     <div className='prompt_card'>
@@ -27,7 +33,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         </div>
 
         <div className='copy_btn'
-          onClick={() => {}}
+          onClick={handleCopy}
         >
           <Image 
             src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
